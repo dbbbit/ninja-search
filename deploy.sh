bb83ebe7f68c9c78e86e9f8502310c6430177d93
GreenFaith <godyycheng@gmail.com>
2014-08-21 13:52:30 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+sudo apt-get install default-jre
+sudo apt-get install default-jdk
+cd ..
+curl -L -O https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.0.tar.gz
+tar -xvf elasticsearch-1.3.0.tar.gz
+cd elasticsearch-1.3.0/bin
+./elasticsearch -f
