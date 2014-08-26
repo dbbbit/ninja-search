349cfea92009fc2f78b5f8f04eaf755c27b55a1b
GreenFaith <godyycheng@gmail.com>
2014-08-26 11:43:19 +0800

    fix css

--- util/page.py
+++ util/page.py
@@ -5,7 +5,7 @@ def gen_pages(current, max_page):
             return x
 
     return [x for x in map(nature, 
-                            range(current-5, current) + range(current, current+6)) 
+                            range(current-3, current) + range(current, current+4)) 
                 if x is not None and x <= max_page]
 
 if __name__ == "__main__":
