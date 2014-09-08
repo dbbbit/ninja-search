c69ebe16f6d9acabd275026f309e5687889d6ed7
dbbbit <dbbbit@gmail.com>
2014-09-08 10:32:18 +0800

    uuu

--- deploy/uwsgi_sock.sh
+++ deploy/uwsgi_sock.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mkdir -p /var/log/ninja-search
+sudo mkdir -p /var/log/ninja-search
 sudo uwsgi -s /tmp/uwsgi.sock --module index --callable app --master --processes 4  \
     --daemonize /var/log/ninja-search/uwsgi_server.log
